42423c0bf14d24125ee3e132a172f65c7aff3d23
camlilly07-lang <cam.lilly07@gmail.com>
2025-09-24 18:58:01 -0400

    Create CamRenLilly_assignment_4.py

---
+++ CamRenLilly_assignment_4.py
@@ -0,0 +1,13 @@
+#Copilot was used to help assign the float variable correctly
+student_name = "Cam'Ren Lilly"
+current_gpa = 4.0
+study_hours = 22
+social_points = 28
+stress_level = 48
+
+print("Welcome these are your starting stats:")
+print(f"Name: {student_name}")
+print(f"GPA: {current_gpa}")
+print(f"Study hours: {study_hours}")
+print(f"Social points: {social_points}")
+print(f"Stress level: {stress_level}")
